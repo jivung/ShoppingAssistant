10ee0ffe1613864a342dc84f0317b7bcbef7fdfe
Taavet <taavet89@live.com>
2016-05-23 05:50:13 +0200

    Done

--- app/src/main/java/com/theforce/shoppingassistant/Compass.java
+++ app/src/main/java/com/theforce/shoppingassistant/Compass.java
@@ -62,19 +62,19 @@ public class Compass extends Activity implements SensorEventListener, Connection
 
         @Override
         public void onProviderDisabled(String provider) {
-            // TODO Auto-generated method stub
+
 
         }
 
         @Override
         public void onProviderEnabled(String provider) {
-            // TODO Auto-generated method stub
+
 
         }
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-            // TODO Auto-generated method stub
+
 
         }
     };
